42dd8771bc7f7be82e51a85fb37d94c5a0a3b0af
Alishrf <a.sharifzadeh11@gmail.com>
2020-01-21 15:56:41 +0330

    add packages

---
+++ src/test/java/io/github/alishrf/travel_website/TravelWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alishrf.travel_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
